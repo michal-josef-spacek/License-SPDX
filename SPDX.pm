6218b2f7b714ee6f434e0ee3a836f2fd9319c8a5
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-03-01 12:34:13 +0100

    Add example for check_exception() method

--- SPDX.pm
+++ SPDX.pm
@@ -272,7 +272,7 @@ Returns string.
  check_license():
          Check type '%s' doesn't supported.
 
-=head1 EXAMPLE
+=head1 EXAMPLE1
 
 =for comment filename=check_license_id.pl
 
@@ -302,6 +302,37 @@ Returns string.
 
  # Output for 'BAD':
  # License with id 'BAD' is not supported.
+
+=head1 EXAMPLE2
+
+=for comment filename=check_license_exception_id.pl
+
+ use strict;
+ use warnings;
+
+ use License::SPDX;
+
+ if (@ARGV < 1) {
+         print STDERR "Usage: $0 license_exception_id\n";
+         exit 1;
+ }
+ my $license_exception_id = $ARGV[0];
+
+ # Object.
+ my $obj = License::SPDX->new;
+
+ print 'License exception with id \''.$license_exception_id.'\' is ';
+ if ($obj->check_exception($license_exception_id)) {
+         print "supported.\n";
+ } else {
+         print "not supported.\n";
+ }
+
+ # Output for 'LGPL-3.0-linking-exception':
+ # License exception with id 'LGPL-3.0-linking-exception' is supported.
+
+ # Output for 'BAD':
+ # License exception with id 'BAD' is not supported.
 
 =head1 DEPENDENCIES
 
